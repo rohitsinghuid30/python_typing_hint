356adcaf2afb19890bdb960b6a223e06a271c499
Rohit Singh <rohituid30@gmail.com>
2024-09-03 13:24:08 +0530

    Added app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+# on functions
+def print_list(a: list) -> None:
+    print(a)
+
+
+print_list([1,2,3])
+# print_list("1") # output - app.py:9: error: Argument 1 to "print_list" has incompatible type "str"; expected "list[Any]"  [arg-type]
+# print_list(1) # output - app.py:10: error: Argument 1 to "print_list" has incompatible type "int"; expected "list[Any]"  [arg-type]
+
+
+# on function -. int
+def myfunc(x: int) -> None:
+    print(x)
+
+myfunc("1") # output : app.py:17: error: Argument 1 to "myfunc" has incompatible type "str"; expected "int"  [arg-type]
+myfunc(1)
